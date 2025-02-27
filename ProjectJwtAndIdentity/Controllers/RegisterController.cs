81838e24eddc6e994852260a6cc9d2c5f37f4494
MuratYucedag <yucedagmurat23@gmail.com>
2025-02-27 20:14:22 +0300

    Add project files.

---
+++ ProjectJwtAndIdentity/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectJwtAndIdentity.Entities;
+using ProjectJwtAndIdentity.Models;
+
+namespace ProjectJwtAndIdentity.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.Username,
+                Email = model.Email
+            };
+            await _userManager.CreateAsync(appUser, model.Password);
+            return RedirectToAction("RoleList", "Role");
+        }
+    }
+}
